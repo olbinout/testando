f7d9dc6bb4cab644dbed6d411a37d25b49f708cc
otavio_lb <testeotavio124@gmail.com>
2025-09-01 09:10:37 -0300

    Questões das aulas -ponteior

--- Questões das aulas/quest2.c
+++ Questões das aulas/quest2.c
@@ -3,20 +3,21 @@
 int main (){
 
     int vetor[10];
+    int *pont_vetor = &vetor[0];
     int i;
     int menor, pos;
 
     for (i = 0; i < 10; i++){
         printf("Insira um valor na posição [%d] do vetor: ", i);
-        scanf("%d", &vetor[i]);
+        scanf("%d", (pont_vetor + i));
 
         if (i == 0){
-            menor = vetor[0];
+            menor = *pont_vetor;
             pos = 0;
         }
 
-        else if (vetor[i] < menor){
-            menor = vetor[i];
+        else if (*(pont_vetor + i) < menor){
+            menor = *(pont_vetor + i);
             pos = i;
         }
     }
