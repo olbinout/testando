85dc3dc15041e3887349e4d352676b4c7cf403b2
otavio_lb <testeotavio124@gmail.com>
2025-09-08 10:22:52 -0300

    Mais questões que foram elaboradas durante as aulas

--- Exercicios/questao10.c
+++ Exercicios/questao10.c
@@ -7,8 +7,10 @@ int main (){
     int sala3[] = {5, 5, 5, 10};
 
     int *ponteiro_salas[3] = {sala1, sala2 ,sala3};
-    int tamanho[3] = {sizeof(sala1) / sizeof(sala1[0]), sizeof(sala2) / sizeof(sala2[0]), sizeof(sala3) / sizeof(sala3[0])};
-    
+    int tamanho[3] = {sizeof(sala1) / sizeof(sala1[0]), 
+                      sizeof(sala2) / sizeof(sala2[0]), 
+                      sizeof(sala3) / sizeof(sala3[0])};
+
     int i, c;
 
     for (i = 0; i < 3; i++){
