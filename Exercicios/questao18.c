8d7882792acb21ab624416f6503af8afdefd356d
otavio_lb <testeotavio124@gmail.com>
2025-09-09 08:59:57 -0300

    Questão 18

---
+++ Exercicios/questao18.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    int *vida_inimigo; // Este ponteiro irá guardar o endereço do espaço de memória que você alocará.
+    vida_inimigo = (int *) malloc(1 * sizeof(int)); // Alocando apenas 01 espaço de memória do tipo "int *" para o ponteiro vida inimigo, que irá receber não o conteúdo, mas sim o primeiro endereço desse espaço que foi alocado.
+
+    if (vida_inimigo == NULL){ // Verificando se deu certo ou não a alocação.
+        printf("Erro: memória insuficiente!\n");
+    } else {
+        printf("Acerto: memória suficiente.\n");
+    }
+
+    *vida_inimigo = 80;
+    printf("Um novo inimigo apareceu com %d de vida!\n", *vida_inimigo);
+
+    *vida_inimigo -= 35;
+    printf("O jogador atacou! Vida restante do inimigo: %d\n", *vida_inimigo);
+
+    return 0;
+}
